c71e6fbfe54db21a5e849de9b8e8f41fbad3173f
jiahui <zhoujiahui03@meituan.com>
2019-08-09 14:58:48 +0800

    Other Cell

--- lib/Home/HomePage.dart
+++ lib/Home/HomePage.dart
@@ -30,7 +30,11 @@ class HomeState extends State <HomePage> {
       body: ListView.separated(
         itemCount: 20,
         itemBuilder:(BuildContext context, int index){
-         return listItem(context,index);
+          if (index == 1) {
+            return thereImageCell(context,index);
+          } else {
+            return listItem(context,index);
+          }
         },
         separatorBuilder:(BuildContext context, int index) {
             return Divider(color: Colors.black);
@@ -39,9 +43,9 @@ class HomeState extends State <HomePage> {
     );
   }
 
+
   Widget listItem(BuildContext context, int index) {
 //    NewModel aNew = newsModel[index];
-
   Widget titleImageSection = new Container(
     padding:EdgeInsets.fromLTRB(15, 10, 10, 5),
     child: Row(
@@ -64,26 +68,77 @@ class HomeState extends State <HomePage> {
                   fontWeight:FontWeight.bold ,
                 ),
               ),
-              Text("信息来源自",
-                textAlign: TextAlign.left,
-                maxLines: 1,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[400],
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: Text("信息来源自",
+                  textAlign: TextAlign.left,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[400],
+                  ),
                 ),
               ),
             ],
           ),
         ),
-        new Image.asset("resource/images/avatar.png",width: 80,height: 80,),
+      Container (
+        padding: EdgeInsets.only(left: 5),
+        child:new Image.asset("resource/images/avatar.png",width: 80,height: 80,),
+      )
       ],
     ),
   );
-
   return titleImageSection;
     return ListTile(title: Text("365Nwes"), subtitle: Text("body in ${index}"),);
   }
 
+  Widget thereImageCell(BuildContext context, int index) {
+    Widget imagesSection = new Container(
+      padding: EdgeInsets.fromLTRB(15, 10, 10, 5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("图片Section的标题，此标题最多也是有两行 ",
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            strutStyle: StrutStyle(///设置行间距
+              fontSize: 10.0,
+              height: 1.3,
+            ),
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight:FontWeight.bold ,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Image.asset("resource/images/yoda.png",width: 100,),
+                Image.asset("resource/images/rabbit.png",width: 100,),
+                Image.asset("resource/images/prayer.png",width: 100,),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 5),
+            child: Text("信息来源自",
+              textAlign: TextAlign.left,
+              maxLines: 1,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[400],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    return imagesSection;
+  }
 
 
 }
